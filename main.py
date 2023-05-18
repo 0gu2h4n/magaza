e4c8abf0d046a368a9d6c7122ec27f8ca17df539
Oğuzhan YAVUZ <79802391+0gu2h4n@users.noreply.github.com>
2023-05-18 20:13:19 +0300

    dil
    main dosyası dil desteği implemantasyonu

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from magaza import magaza #Mağaza kütüphanesi/dosyası
+import dil
 
 def main():
     while True:
         magazalar = dict() #Oluşturulan bütün mağazaların tutulacağı sözlük
 
-        print('\nGerekli bilgileri girin... (Çıkış için bilgileri boş bırakın)')
-        magazaadi, saticiadi, saticicinsi = input('Mağaza Adı:'), input('Satıcı Adı:'),input('Satıcı Cinsi:')#Bütün mağaza ve satıcı bilgilerini al
+        print(dil.main()[0])
+        magazaadi, saticiadi, saticicinsi = input(dil.main()[1]), input(dil.main()[2]),input(dil.main()[3])#Bütün mağaza ve satıcı bilgilerini al
 
         if not (magazaadi or saticiadi or saticicinsi): break #Çıkış yapma durumu
 
@@ -16,30 +17,30 @@ def main():
 
         while True:
             try: #Kullanıcıdan geçerli bir satış tutarı almayı dene
-                tutar = float(input('Satış tutarı: '))
+                tutar = float(input(dil.main()[4]))
                 if tutar < 0: raise(Exception) #Satış 0'dan küçük olamaz
                 magazalar[magazaadi].SatisYap(tutar)
                 break
-            except: print('Geçerli bir değer girin!')
-        print('\nKayıt Başarılı!')
+            except: print(dil.main()[5])
+        print(dil.main()[6])
 
     while True:
         #Arama için mağaza adını al
-        mgz = input('\nBilgilerini getirmek istediğiniz mağaza adını girin (Çıkış için boş bırakın): ')
+        mgz = input(dil.main()[7])
         #Çıkış
         if not mgz: break
         #Bu adla kayıtlı bir mağaza var mı?
         if mgz not in magazalar:
-            print('\nMağaza Bulunamadı!')
+            print(dil.main()[8])
             continue
         #Bir satıcının satışları için satıcı adı al ya da bütün mağaza için
-        stc = input('\nSatışlarını getirmek istediğiniz satıcı adını girin (Bütün mağaza için boş bırakın): ')
+        stc = input(dil.main()[9])
         #Bütün mağaza için
         if not stc: print(f'\n{magazalar[mgz]}')
         #Satıcı için
         else: 
             sts = magazalar[mgz].SatisAl(stc)
-            if sts != -1.0: print(f'\nSatıcı toplam satış tutarı: {sts}') 
+            if sts != -1.0: print(f'{dil.main()[10]} {sts}') 
 
 
 

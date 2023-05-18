7f42326c2e71279f8a629c437039ac312bec211c
Oğuzhan YAVUZ <79802391+0gu2h4n@users.noreply.github.com>
2023-05-18 20:01:27 +0300

    kayıt işlevi
    main dosyasına mağaza, satıcı ve satış kayıt işlevi

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from magaza import magaza #Mağaza kütüphanesi/dosyası
 
 def main():
-    pass
+    while True:
+        magazalar = dict() #Oluşturulan bütün mağazaların tutulacağı sözlük
+
+        print('\nGerekli bilgileri girin... (Çıkış için bilgileri boş bırakın)')
+        magazaadi, saticiadi, saticicinsi = input('Mağaza Adı:'), input('Satıcı Adı:'),input('Satıcı Cinsi:')#Bütün mağaza ve satıcı bilgilerini al
+
+        if not (magazaadi or saticiadi or saticicinsi): break #Çıkış yapma durumu
+
+        if magazaadi not in magazalar: magazalar[magazaadi] = magaza(magazaadi, saticiadi, saticicinsi) #Şu anki mağaza mağazalar sözlüğü içerisinde yoksa oluştur
+        else: #Mağaza hali hazırda varsa satıcı adını ve cinsini değiştir
+            magazalar[magazaadi].SatAdYap(saticiadi)
+            magazalar[magazaadi].SatCinYap(saticicinsi)
+
+        while True:
+            try: #Kullanıcıdan geçerli bir satış tutarı almayı dene
+                tutar = float(input('Satış tutarı: '))
+                if tutar < 0: raise(Exception) #Satış 0'dan küçük olamaz
+                magazalar[magazaadi].SatisYap(tutar)
+                break
+            except: print('Geçerli bir değer girin!')
+        print('\nKayıt Başarılı!')
+
 
 if __name__ == "__main__": main()
